70250ea368a041221d312f798c663778cd72d951
Jaroslav <jaroslavskudrna@gmail.com>
2020-06-02 13:27:45 +0200

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Shapes from "././Shapes"
 
 function App() {
+  const [paint, setPaint] = useState([
+    {
+      node: 0,
+      xValue: 200,
+      yValue: 80,
+      shapes: [
+        {
+          id: 1,
+          type: "line",
+          x1: 0,
+          y1: 80,
+          x2: 200,
+          y2: 80,
+          stroke: "black",
+        },
+        { id: 2, type: "rect", x: 0, y: 80, width: 100, height: 100 },
+      ],
+    },
+  ])
+  // const [xValue, setXVaue] = useState(200)
+  let xValue = 200
+  const initialNode = 0
+  function addRectangle() {
+    const newX = xValue + 100
+    const newX2 = newX + 100
+    const savedX = xValue + newX
+    const newShapes = [
+      ...paint[initialNode].shapes,
+      {
+        id: 1,
+        type: "line",
+        x1: xValue,
+        y1: 80,
+        x2: newX2,
+        y2: 80,
+        stroke: "black",
+      },
+    ]
+    console.log(newShapes)
+    console.log("This is the state paint", paint)
+    setPaint({
+      ...paint,
+      [initialNode]: {
+        ...paint[initialNode],
+        xValue: savedX,
+        shapes: newShapes,
+      },
+    })
+    // setPaint({
+    //   ...paint,
+    //   [initialNode]: {
+    //     ...paint[initialNode],
+    //     value: savedX,
+    //   },
+    // })
+    // setXVaue(newX)
+    // xValue = xValue + newX
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={addRectangle}>Add rectangle</button>
+      <Shapes paint={paint} node={0} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
